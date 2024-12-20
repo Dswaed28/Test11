34214a894a8e27a451baf94ca7d50a8c3f86d3e4
Dswaed28 <daniaswaed4@gmail.com>
2024-12-20 15:23:00 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-//Task 1
-const num = 60;
-//a
-Number(num % 5 === 0 || num % 2 === 0 || num % 3 === 0) && console.log("1");
-//b
-Number(
-  (num % 2 === 0 && num % 3 === 0) ||
-    (num % 3 === 0 && num % 5 === 0) ||
-    (num % 2 === 0 && num % 5 === 0)
-) && console.log("2");
-//c
-Number(num % 2 === 0 && num & (3 === 0) && num % 5 === 0) || console.log("3");
+const fs = require("fs");
+const path = require("path");
+let newFile = "";
+const files = fs.readdirSync(__dirname);
+const txtFiles = files.filter((file) => file.split(".")[1] === "txt");
+txtFiles.forEach((file, index) => {
+  const linesToRead = index + 1;
+  const filePath = path.join(__dirname, file);
+  const fileContent = fs.readFileSync(filePath, "utf8");
+  const lines = fileContent.split("\n");
+  for (let i = 0; i < linesToRead && i < lines.length; i++) {
+    newFile += lines[i] + "\n";
+  }
+});
+
+fs.writeFileSync("output.txt", newFile);
